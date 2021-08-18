febf2c0cdfaf7d0efaf4fdb021c9f0eb4006b6da
Hung Nguyen <mrhungton@gmail.com>
2021-08-19 03:42:56 +0700

    Setup tokens

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :users, only: [:create, :update, :destroy] do
+      resources :tokens, only: [:create]
+      resources :users, only: [:show, :create, :update, :destroy] do
         collection do
           get '/' => 'users#list'
         end
